8bd0f79adeadcd3875a917b0b7e60c09d9b680b3
catwithtudou <949812478@qq.com>
2023-12-19 11:05:06 +0800

    update:update the socket

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,7 @@
-use bytes::Bytes;
+use bytes::{Bytes, BytesMut};
 use log::debug;
 use std::sync::Arc;
+use std::time::Duration;
 use std::{
     borrow::Cow,
     io,
@@ -98,9 +99,11 @@ fn error_invalid_input<T>(msg: &'static str) -> io::Result<T> {
 }
 
 impl Client {
+    // from_socket 处理iptables转发的请求和client主动建联请求
     pub async fn from_socket(mut peer_left: TcpStream, config: Arc<Config>) -> io::Result<Self> {
         let left_src = peer_left.peer_addr()?;
         let src_port = peer_left.local_addr()?.port();
+        // 获取原始目的地
         let dest = get_original_address_v4(&peer_left)
             .map(SocketAddr::V4)
             .or_else(|_| get_original_address_v6(&peer_left).map(SocketAddr::V6))
@@ -115,17 +118,96 @@ impl Client {
         let dest = if cfg!(target_os = "linux") && is_nated {
             dest.into()
         } else {
-            // TODO
-            dest.into()
+            // 根据协议获取信息
+            // Client 给出支持的握手协议
+            let ver = peer_left.read_u8().await?;
+            if ver != 0x05 {
+                return error_invalid_input("Neither a NATed or SOCKSv5 connection");
+            }
+            let n_methods = peer_left.read_u8().await?;
+            let mut buf = vec![0u8; n_methods as usize];
+            peer_left.read_exact(&mut buf).await?;
+            if buf.iter().find(|&&m| m == 0).is_none() {
+                return error_invalid_input("Socksv5, Only no auth supported");
+            }
+            peer_left.write_all(&[0x05, 0x00]).await?;
+            buf.resize(4, 0);
+            peer_left.read_exact(&mut buf).await?;
+            if buf[0..2] != [0x05, 0x01] {
+                return error_invalid_input("Socksv5, CONNECT is required");
+            }
+            // Client 给出真实目的地
+            let addr: Address = match buf[3] {
+                0x01 => {
+                    // ipv4
+                    let mut buf = [0u8; 4];
+                    peer_left.read_exact(&mut buf).await?;
+                    buf.into()
+                }
+                0x03 => {
+                    // domain
+                    let domain_len = peer_left.read_u8().await? as usize;
+                    buf.resize(domain_len, 0);
+                    let raw_ipv4 = peer_left.read_exact(&mut buf).await?;
+                    let domain = String::from_utf8(buf).map_err(|_| {
+                        io::Error::new(io::ErrorKind::InvalidInput, "Socksv5, invalid domain name")
+                    })?;
+                    domain.into()
+                }
+                0x04 => {
+                    // ipv6
+                    let mut buf = [0u8; 16];
+                    peer_left.read_exact(&mut buf).await?;
+                    buf.into()
+                }
+                _ => return error_invalid_input("Socksv5, unknown adress type"),
+            };
+            let port = peer_left.read_u16().await?;
+            peer_left.write_all(&[5, 0, 0, 1, 0, 0, 0, 0, 0, 0]).await?;
+            (addr, port).into()
         };
 
         Ok(Client {
             dest,
-            config: config,
+            config,
             from_port: src_port,
             left: peer_left,
             src: left_src,
             pending_data: None,
+        })
+    }
+}
+
+impl Client {
+    // retrieve_dest 获取 Dest 信息
+    // REDIRECT 情况下不会有 socks 的握手流程，起手流量是 TLS client hello，则需要我们从 TLS 嗅探出 domain name，用于做 DNS 远程解析
+    pub async fn retrieve_dest(self) -> io::Result<Client> {
+        let Client {
+            mut left,
+            src,
+            mut dest,
+            from_port,
+            config,
+            pending_data,
+        } = self;
+        let wait = Duration::from_millis(500);
+        let mut buf = BytesMut::with_capacity(2048);
+        let mut pending_data = None;
+        buf.resize(buf.capacity(), 0);
+        if let Ok(len) = timeout(wait, left.read(&mut buf)).await? {
+            // 只保留读出的数据，丢弃其他数据
+            // 这样保证往 socket 回写时不会写入初始化时的 0
+            buf.truncate(len);
+            // TODO:TLS处理
+        }
+
+        Ok(Client {
+            from_port,
+            dest,
+            left,
+            src,
+            pending_data,
+            config,
         })
     }
 }
